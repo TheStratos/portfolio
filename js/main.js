ff200b9827bb09d9eaf350cd06906da6b998b87e
TheStratos <167118430+TheStratos@users.noreply.github.com>
2024-05-10 13:37:23 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -37,18 +37,71 @@ const themeIsGreen = document.querySelector(".themetoggle__green");
 themeIsGreen.addEventListener("click", () => {
         document.body.className = '';
         document.body.classList.add("theme-green");
-        
+
 
 })
 const themeIsBlue = document.querySelector(".themetoggle__blue");
 themeIsBlue.addEventListener("click", () => {
         document.body.className = '';
         document.body.classList.add("theme-blue");
-        
+
 })
 const themeIsOrange = document.querySelector(".themetoggle__orange");
 themeIsOrange.addEventListener("click", () => {
         document.body.className = '';
         document.body.classList.add("theme-orange");
-        
+
 })
+
+
+
+const autoText = document.querySelector(".auto__text");
+const skills = ['HTML', 'CSS', 'JS', 'React'];
+let skillIndex = 0;
+let characterIndex = 0;
+const writeText = () => {
+        let interval = setInterval(() => {
+                characterIndex++;
+                autoText.textContent = `${skills[skillIndex].slice(0, characterIndex)}`;
+                if (characterIndex === skills[skillIndex].length) {
+                        // setTimeout(() => {
+                        //         skillIndex++;
+                        //         characterIndex = 0;
+                        //         if (skillIndex === skills.length) {
+                        //                 skillIndex = 0;
+                        //         }
+                        // }, 1000);
+                        setTimeout(() => {
+                                clearInterval(interval);
+                                removeText();
+                        }, 500);
+                };
+
+        }, 250);
+};
+
+writeText();
+
+
+const removeText = () => {
+        let interval = setInterval(() => {
+                if (autoText.textContent.length) {
+
+                        characterIndex--;
+                        autoText.textContent = `${skills[skillIndex].slice(0, characterIndex)}`;
+
+                } else {
+                        clearInterval(interval);
+                        skillIndex++;
+                        if (skillIndex === skills.length) {
+                                skillIndex = 0;
+                        }
+                        writeText();
+
+                }
+
+
+        }, 200)
+}
+
+
